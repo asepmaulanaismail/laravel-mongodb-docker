9178104ce57e6c6913894192c62fd0e88fe073a7
Asep Maulana Ismail <asepmaulanaismail@gmail.com>
2018-01-17 23:58:42 -0800

    Initial commit

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-apt-get install -y composer php7.0-mbstring php7.0-xml
-chmod 777 storage/logs
-chmod 777 bootstrap/cache
+apt-get install -y composer php7.0-mbstring php7.0-xml php-mongodb
+chmod 777 storage/logs/*
+chmod 777 storage/framework/views/*
+chmod 777 bootstrap/cache/*
 cp .env.example .env
 php artisan key:generate
