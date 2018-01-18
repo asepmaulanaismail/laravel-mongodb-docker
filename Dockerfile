671f7fd2a2e0529399e5fd320750dfdeacd11f74
Asep Maulana Ismail <asepmaulanaismail@gmail.com>
2018-01-17 20:22:25 -0800

    Bugs fixing

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,2 @@
 FROM creativitykills/nginx-php-server:latest
 MAINTAINER Asep Maulana Ismail <asepmaulanaismail@gmail.com>
-
-RUN php artisan key:generate
--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ apt-get install -y composer php7.0-mbstring php7.0-xml
 chmod 777 storage/logs
 chmod 777 bootstrap/cache
 cp .env.example .env
+php artisan key:generate
